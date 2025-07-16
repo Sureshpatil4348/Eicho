713ecb9b0a8a5ce425160d2128a69dbd4ed69f22
soumallyadey2024 <soumallya.dey@technoexponent.com>
2025-07-16 19:12:45 +0530

    FIx some build error

--- src/renderer/src/services/actions/modal.action.ts
+++ src/renderer/src/services/actions/modal.action.ts
@@ -1,11 +1,11 @@
 import { Dispatch } from "redux";
 import { MODAL_ACTION, MODAL_FEATURES } from "../constants/modal.constant";
-import { MODAL_PAYLOAD } from "@/types/modal.type";
+import { MODAL_PAYLOAD } from "@renderer/types/modal.type";
 
-export const openModal = (data: MODAL_PAYLOAD, dispatch: Dispatch<MODAL_ACTION>) => {
-    dispatch({ type: MODAL_FEATURES.MODAL_OPEN, payload: data })
+export const openModal = (data: MODAL_PAYLOAD, dispatch: Dispatch<MODAL_ACTION>): void => {
+  dispatch({ type: MODAL_FEATURES.MODAL_OPEN, payload: data })
 }
 
-export const closeModal = (dispatch: Dispatch<MODAL_ACTION>) => {
-    dispatch({ type: MODAL_FEATURES.MODAL_CLOSE })
+export const closeModal = (dispatch: Dispatch<MODAL_ACTION>): void => {
+  dispatch({ type: MODAL_FEATURES.MODAL_CLOSE })
 }
