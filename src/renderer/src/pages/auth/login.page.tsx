144a8afa2e64055c2e802f0f22ca7f1050dae596
soumallyadey2024 <soumallya.dey@technoexponent.com>
2025-07-21 19:20:19 +0530

    Fix some build issues and others

--- src/renderer/src/pages/auth/login.page.tsx
+++ src/renderer/src/pages/auth/login.page.tsx
@@ -1,16 +1,4 @@
 import React from "react";
-import {
-  Box,
-  Card,
-  CardContent,
-  Typography,
-  TextField,
-  InputAdornment,
-  Button,
-  Link as MUILink,
-} from "@mui/material";
-import EmailIcon from "@mui/icons-material/Email";
-import LockIcon from "@mui/icons-material/Lock";
 import { Link } from "react-router-dom";
 import Logo from "@renderer/assets/images/logo.svg";
 import Email from "@renderer/assets/images/email-icon.svg";
@@ -18,62 +6,12 @@ import Password from "@renderer/assets/images/password-icon.svg";
 
 const LoginPage: React.FunctionComponent = () => {
   return (
-    <>
-      {/* <Box sx={{ height: '100vh', backgroundColor: '#eaf1f7', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <Box sx={{ mb: 3 }}>
-          <img src={Logo} alt="Forex Logo" style={{ height: 50 }} />
-        </Box>
-
-        <Card sx={{ width: 400, borderRadius: 3, boxShadow: 3, height: 500 }}>
-          <CardContent sx={{ px: 4, py: 5 }}>
-            <Typography variant="h6" align="center" gutterBottom>
-              Login
-            </Typography>
-            <Typography variant="body2" align="center" color="textSecondary" sx={{ mb: 3 }}>
-              Login With Your Personal Details
-            </Typography>
-
-            <TextField fullWidth variant="outlined" label="Email" type="email" sx={{ mb: 2, backgroundColor: '#f1f7fc' }} InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <EmailIcon />
-                </InputAdornment>
-              )
-            }} />
-
-            <TextField fullWidth variant="outlined" label="Password" type="password" sx={{ mb: 1.5, backgroundColor: '#f1f7fc' }} InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockIcon />
-                </InputAdornment>
-              )
-            }} />
-
-            <Box display="flex" justifyContent="flex-end" mb={2}>
-              <MUILink component={Link} to="#" underline="hover" variant="body2" color="primary">
-                Forgot Password
-              </MUILink>
-            </Box>
-
-            <Button fullWidth variant="contained" color="primary" size="large">
-              Login
-            </Button>
-          </CardContent>
-        </Card>
-
-        <Typography variant="body2" sx={{ mt: 2 }}>
-          Don’t Have an account?{' '}
-          <MUILink component={Link} to="#" underline="hover" color="primary">
-            Signup
-          </MUILink>
-        </Typography>
-      </Box> */}
       <div className="auth_page">
         <div className="before_login_sec">
           <div className="logo">
-            <a href="/">
+            <Link to="/">
               <img src={Logo} alt="Forex Logo" style={{ height: 50 }} />
-            </a>
+            </Link>
           </div>
           <div className="wrap_box">
             <div className="head">
@@ -100,7 +38,7 @@ const LoginPage: React.FunctionComponent = () => {
                 </div>
               </div>
               <div className="form-group forgot_password">
-                <a href="#">Forgot Password</a>
+                <Link to="#">Forgot Password</Link>
               </div>
               <div className="form-group text-center">
                 <button type="button" className="login">
@@ -111,12 +49,11 @@ const LoginPage: React.FunctionComponent = () => {
           </div>
           <div className="allready_account">
             <span>
-              Don't Have an account? <a href="#">Signup</a>
+              Don&apos;t Have an account? <Link to="#">Signup</Link>
             </span>
           </div>
         </div>
       </div>
-    </>
   );
 };
 
