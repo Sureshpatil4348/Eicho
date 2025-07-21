9d44d0a21ec3aa406b184d476bffb5a9bbde48cd
Surojit <surojitdas@technoexponent.com>
2025-07-21 12:16:45 +0530

    create new page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,6 +3,7 @@ import { AuthGuard, DashboardGuard } from './guards'
 import LoginPage from './pages/auth/login.page'
 import HomePage from './pages/dashboard/home/home.page'
 import PortfolioPage from './pages/dashboard/portfolio/portfolio.page'
+import AccountPage from './pages/dashboard/account/account.page'
 
 function App(): React.JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
@@ -16,6 +17,7 @@ function App(): React.JSX.Element {
       <Route path='/dashboard' element={<DashboardGuard />}>
         <Route index element={<HomePage />} />
         <Route path='portfolio' element={<PortfolioPage />} />
+        <Route path='account' element={<AccountPage />} />
       </Route>
     </Routes>
   )
--- src/renderer/src/pages/dashboard/account/account.page.tsx
+++ src/renderer/src/pages/dashboard/account/account.page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PortfolioPage: React.FunctionComponent = () => {
   return (
     <div>
-      Portfolio
+      Account
     </div>
   )
 }
