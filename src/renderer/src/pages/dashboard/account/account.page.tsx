ab56687bc64823ab1a9c2c1fb82ba3af97fff713
Surojit <surojitdas@technoexponent.com>
2025-07-21 13:40:36 +0530

    account page

--- src/renderer/src/pages/dashboard/account/account.page.tsx
+++ src/renderer/src/pages/dashboard/account/account.page.tsx
@@ -1,10 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PortfolioPage: React.FunctionComponent = () => {
   return (
-    <div>
-      Account
-    </div>
+    <>
+      <div className="account_main_sec">
+        <div className="account_main_box">
+          <div className="account_top">
+            <div className="account_image_wrap">
+              <div className="account_image">
+
+              </div>
+              <div className='upload_image'>
+
+              </div>
+            </div>
+            <div className="account_details">
+              <h3>John Simmons</h3>
+              <ul>
+                <li>
+                  <span>(316) 555-0116</span>
+                </li>
+                <li>
+                  <span>jackson.graham@example.com</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="account_bottom">
+            <div className="list_wrap">
+              <div className="list">
+                <Link to="/">
+                  <div className="left">
+                    <div className="icon">
+
+                    </div>
+                    <span>My Profile</span>
+                  </div>
+                  <div className="right">
+
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
